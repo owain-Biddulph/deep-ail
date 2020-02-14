f062bcecd4283da8fa2dcff8e382bcb845e3ac7c
Owain Biddulph <owainbiddulph@Owains-MacBook-Pro.local>
2020-02-14 12:00:25 +0100

    Add main

---
+++ main.py
@@ -0,0 +1,21 @@
+from client import ClientSocket
+import config
+from state import State
+from response import response
+
+
+def main():
+    client_socket = ClientSocket()
+    client_socket.send_message(("nme", "DeepAil"))
+    # set message
+    client_socket.get_message()
+    state = State(client_socket.message)
+    # hum message
+    client_socket.get_message()
+    state.update(client_socket.message)
+    # hme message
+    client_socket.get_message()
+    state.update(client_socket.message)
+    # map message
+    client_socket.get_message()
+    state.update(client_socket.message)
