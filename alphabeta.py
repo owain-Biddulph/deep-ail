feebe784cfd9d9a6b21135ad2e4b68dcaf3a3a14
François <Franois@gitkraken.com>
2020-02-14 22:00:31 +0100

    création méthode vampire/werewolves/humans et début de la méthode accessible states

--- alphabeta.py
+++ alphabeta.py
@@ -1,29 +1,23 @@
+
+
 def MaxValue(state, alpha, beta):
-    boucle = True
-    k=0
-    while boucle:
-        if k == 10:
-            return eval(state)
-        for states in successeurs(state):
-            alpha = max(alpha, MinValue(states, alpha, beta))
-            if alpha >= beta:
-                return beta
-        return alpha
-    boucle = False
+    if successeurs(state) == None:
+        return eval(state)
+    for states in successeurs(state):
+        alpha = max(alpha, MinValue(states, alpha, beta))
+        if alpha >= beta:
+            return beta
+    return alpha
 
 
 def MinValue(state, alpha, beta):
-    boucle = True
-    k=0
-    while boucle:
-        if k == 10:
-            return eval(state)
-        for states in successeurs(state):
-            alpha = min(alpha, MaxValue(states, alpha, beta))
-            if alpha >= beta:
-                return beta
-        return alpha
-    boucle = False
+    if successeurs(state) == None:
+        return eval(state)
+    for states in successeurs(state):
+        alpha = min(alpha, MaxValue(states, alpha, beta))
+        if alpha >= beta:
+            return beta
+    return alpha
 ##Todo
 ## faire les states, et  calculer les states successeurs, et eval (coté nico avec l'heuristique)
 
--- state.py
+++ state.py
@@ -39,3 +39,58 @@ class State:
                     species: int = 0
                     self._board[change[0], change[1], 1] = 0
                 self._board[change[0], change[1], 0] = species
+
+    ##renvoie la liste des cases où sont situés les vampires et leur nombre
+    @property
+    def vampire(self) -> List:
+        vampire_list = []
+        for i in range(self._nb_rows):
+            for j in range(self._nb_columns):
+                if self._board[i, j, 0] == 0: ##verifier que c'est bien cette valeur pour les vampires
+                    vampire_list.append([i, j, self._board[i, j, 1]])
+        return vampire_list
+
+    ##renvoie la liste des cases où sont situés les werewolves et leur nombre
+    @property
+    def werewolf(self) -> List:
+        werewolf_list = []
+        for i in range(self.nb_rows):
+            for j in range(self.nb_columns):
+                if self._board[i, j, 0] == 1: ##verifier que c'est bien cette valeur pour les werewolves
+                    werewolf_list.append([i, j, self._board[i, j, 1]])
+        return werewolf_list
+
+    ##renvoie la liste des cases où sont situés les humains et leur nombre
+    @property
+    def human(self) -> List:
+        human_list = []
+        for i in range(self.nb_rows):
+            for j in range(self.nb_columns):
+                if self._board[i, j, 0] == 2: ##verifier que c'est bien cette valeur pour les humans
+                    human_list.append([i, j, self._board[i, j, 1]])
+        return human_list
+
+    ##renvoie la liste des mouvements possibles depuis notre state, et en format message (pour qu'on puisse le mettre en entrée de la méthode update dans accessible_state)
+    def accessible_moves(self):     ##je fais pour les vampires au débuts
+        moves = []
+        vampires = self.vampire()
+        for vampire in vampires:
+            possible_moves = vampire
+
+        return moves
+
+    ##renvoie la liste des boards accessibles depuis le state actuel
+    def accessible_states(self):
+        states = []
+        moves = self.accessible_moves()
+        for move in moves:
+            state = State(( "???", (self._nb_rows, self._nb_columns))) ##qu'est ce que prend state en entrée
+            state._board = self._board
+            states.append(state.update(move))
+        return states
+
+
+
+
+
+
