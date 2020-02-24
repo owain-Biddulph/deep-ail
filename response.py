e2d9dafc5a082bdd0a8104b10cca456da5f01c15
Owain Biddulph <owainbiddulph@Owains-MacBook-Pro.local>
2020-02-24 13:04:45 +0100

    Add possible moves function

--- response.py
+++ response.py
@@ -1,3 +1,4 @@
+from itertools import permutations
 from typing import Tuple, List
 
 from state import State
@@ -11,3 +12,60 @@ def respond(state: State) -> Tuple[int, List]:
             if state.board[x, y, 0] == state.our_species:
                 moves += [[x, y, state.board[x, y, 1], max(0, x-1), y]]
     return len(moves), moves
+
+
+def all_possible_moves(state: State) -> List:
+    our_squares = list(zip(*state.board[:, :, 0].where(arr=state.our_species)))
+    possible_moves = []
+    for square in our_squares:
+        x, y = square
+        nb_units = state.board[x, y, 1]
+        # Ugly but I think it is probably the fastest way
+        # TODO: check
+        if x == 0:
+            if y == 0:
+                possible_moves.append([
+                    (x, y, nb_units, x + 1, y),
+                    (x, y, nb_units, x + 1, y + 1),
+                    (x, y, nb_units, x, y + 1),
+                ])
+            elif y == state.nb_rows - 1:
+                possible_moves.append([
+                    (x, y, nb_units, x + 1, y),
+                    (x, y, nb_units, x + 1, y - 1),
+                    (x, y, nb_units, x, y - 1),
+                ])
+            else:
+                possible_moves.append([
+                    (x, y, nb_units, x + 1, y + 1),
+                    (x, y, nb_units, x + 1, y),
+                    (x, y, nb_units, x + 1, y - 1),
+                    (x, y, nb_units, x, y + 1),
+                    (x, y, nb_units, x, y - 1),
+                ])
+        elif x == state.nb_columns - 1:
+            if y == 0:
+                possible_moves.append([
+                    (x, y, nb_units, x - 1, y),
+                    (x, y, nb_units, x - 1, y + 1),
+                    (x, y, nb_units, x, y + 1),
+                ])
+            elif y == state.nb_rows:
+                possible_moves.append([
+                    (x, y, nb_units, x - 1, y),
+                    (x, y, nb_units, x - 1, y - 1),
+                    (x, y, nb_units, x, y - 1),
+                ])
+            else:
+                possible_moves.append([
+                    (x, y, nb_units, x - 1, y + 1),
+                    (x, y, nb_units, x - 1, y),
+                    (x, y, nb_units, x - 1, y - 1),
+                    (x, y, nb_units, x, y + 1),
+                    (x, y, nb_units, x, y - 1),
+                ])
+        else:
+            pos = set(permutations([-1, -1, 0, 1, 1]))
+            possible_moves += [(x, y, nb_units, x - t, y - u) for (t, u) in pos]
+
+        return possible_moves
