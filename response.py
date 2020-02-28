1b173a775c6ba0d72d290e0d09bdf91014bb1773
Barrau Stepan <stepan.barrau@student.ecp.fr>
2020-02-28 11:16:50 +0100

    fix

--- response.py
+++ response.py
@@ -2,6 +2,7 @@ from itertools import permutations
 import numpy as np
 from typing import Tuple, List
 import time
+import random
 
 from state import State
 
@@ -9,18 +10,16 @@ from state import State
 def respond(state: State) -> Tuple[int, List]:
     # Basic test, always moves left
     moves = []
-    print(all_possible_moves(state))
+    possible_moves = all_possible_moves(state)
     time.sleep(0.5)
-    for x in range(state.nb_rows):
-        for y in range(state.nb_columns):
-            if state.board[x, y, 0] == state.our_species:
-                moves += [[x, y, state.board[x, y, 1], max(0, x-1), y]]
+    moves.append(random.choice(possible_moves))
+    print(moves)
     return len(moves), moves
 
 
 def all_possible_moves(state: State) -> List:
     our_squares = list(zip(*np.where(state.board[:, :, 0] == state.our_species)))
-    possible_moves = []
+    possible_moves = None
     for square in our_squares:
         x, y = square
         nb_units = state.board[x, y, 1]
@@ -28,11 +27,11 @@ def all_possible_moves(state: State) -> List:
         # TODO: check
         if x == 0:
             if y == 0:
-                possible_moves.append([
+                possible_moves = [
                     (x, y, nb_units, x + 1, y),
                     (x, y, nb_units, x + 1, y + 1),
                     (x, y, nb_units, x, y + 1),
-                ])
+                ]
             elif y == state.nb_rows - 1:
                 possible_moves.append([
                     (x, y, nb_units, x + 1, y),
@@ -40,36 +39,36 @@ def all_possible_moves(state: State) -> List:
                     (x, y, nb_units, x, y - 1),
                 ])
             else:
-                possible_moves.append([
+                possible_moves = [
                     (x, y, nb_units, x + 1, y + 1),
                     (x, y, nb_units, x + 1, y),
                     (x, y, nb_units, x + 1, y - 1),
                     (x, y, nb_units, x, y + 1),
                     (x, y, nb_units, x, y - 1),
-                ])
+                ]
         elif x == state.nb_columns - 1:
             if y == 0:
-                possible_moves.append([
+                possible_moves = [
                     (x, y, nb_units, x - 1, y),
                     (x, y, nb_units, x - 1, y + 1),
                     (x, y, nb_units, x, y + 1),
-                ])
-            elif y == state.nb_rows:
-                possible_moves.append([
+                ]
+            elif y == state.nb_rows - 1:
+                possible_moves = [
                     (x, y, nb_units, x - 1, y),
                     (x, y, nb_units, x - 1, y - 1),
                     (x, y, nb_units, x, y - 1),
-                ])
+                ]
             else:
-                possible_moves.append([
+                possible_moves = [
                     (x, y, nb_units, x - 1, y + 1),
                     (x, y, nb_units, x - 1, y),
                     (x, y, nb_units, x - 1, y - 1),
                     (x, y, nb_units, x, y + 1),
                     (x, y, nb_units, x, y - 1),
-                ])
+                ]
         else:
             pos = set(permutations([-1, -1, 0, 1, 1], 2))
-            possible_moves += [(x, y, nb_units, x - t, y - u) for (t, u) in pos]
+            possible_moves = [(x, y, nb_units, x - t, y - u) for (t, u) in pos]
 
         return possible_moves
