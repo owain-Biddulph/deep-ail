d5ea79b555bc48b613931581a36b1eed84c1fba0
François <Franois@gitkraken.com>
2020-03-23 09:41:18 +0100

    ajout de la stratégie de foncer sur l'adversaire

--- systeme_expert/strategies.py
+++ systeme_expert/strategies.py
@@ -13,7 +13,7 @@ class AttackFirst(Strategy):
 
     def play(self, state):
         distance_min, groups = utils.distance_min(state)
-        print("distance_min:",distance_min)
+        print("distance_min:", distance_min)
         if distance_min == 1:
             moves = [[groups[0][0], groups[0][1], state.board[groups[0][0], groups[0][1], 1],groups[1][0], groups[1][1]]]
 
@@ -23,7 +23,7 @@ class AttackFirst(Strategy):
                 new_distance = utils.distance(square, groups[1])
                 moves = [[groups[0][0], groups[0][1], state.board[groups[0][0], groups[0][1], 1], square[0], square[1]]]
                 if new_distance == 2:
-                    print("move choisi", moves)
+                    print("move choisi", move)
                     break
 
         else:
@@ -33,14 +33,27 @@ class AttackFirst(Strategy):
                 new_distance = utils.distance(square, groups[1])
 
                 if new_distance < min_distances:
-                    moves = [
+                    move = [
                         [groups[0][0], groups[0][1], state.board[groups[0][0], groups[0][1], 1], square[0], square[1]]]
                     min_distances = new_distance
         time.sleep(0.3)
-        return moves
+        return move
 
 
+class StraightAttack(Strategy):
 
+    def play(self, state):
+
+        distance_min, groups = utils.distance_min(state)
+        possible_squares = possible_target_squares(state.nb_rows, state.nb_columns, groups[0][0], groups[0][1])
+
+        for square in possible_squares:
+            new_distance = utils.distance(square, groups[1])
+            move = [[groups[0][0], groups[0][1], state.board[groups[0][0], groups[0][1], 1], square[0], square[1]]]
+            if new_distance == distance_min - 1:
+                print("move choisi", move)
+                break
+        return move
 
 
 
