8a177cecf47b40f5ce133971af50c11377aad640
Owain Biddulph <owainbiddulph@Owains-MacBook-Pro.local>
2020-03-29 11:24:13 +0200

    Return best move after 1.9s, reduce depth if needed

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import time
 from client import ClientSocket
 from heuristics.basic import Heuristic
 from state import State
-from response import respond
+from response import GlobalStrategy
 from argparse import ArgumentParser
 
 
@@ -25,12 +25,14 @@ def play_game(strategy, args):
 
     # start of the game
     heuristic = Heuristic()
+
     while True:
         client_socket.get_message()
+        time_message_received = time.time()
         state.update(client_socket.message)
         if client_socket.message[0] == "upd":
             t2 = time.time()
-            nb_moves, moves = strategy(state, heuristic)
+            nb_moves, moves = strategy.respond(state, time_message_received)
             t3 = time.time()
             print(f"time to think about strategy : {t3 - t2}")
             client_socket.send_mov(nb_moves, moves)
@@ -43,5 +45,6 @@ if __name__ == '__main__':
     parser.add_argument(dest='port', default='5555', type=int, help='Chosen port for the connection.')
     
     args = parser.parse_args()
-    
-    play_game(respond, args)
+    heuristic = Heuristic()
+    strategy = GlobalStrategy(4, heuristic)
+    play_game(strategy, args)
--- response.py
+++ response.py
@@ -1,16 +1,22 @@
 from typing import Tuple, List
 from state import State
-from alphabeta import alphabeta
+from alphabeta import alphabeta, TimeError
 from math import inf
 
 
-def respond(state: State, heuristic) -> Tuple[int, List]:
-    state_copy = state.copy_state()
-    times = [0, 0, 0]
-    _, moves, times = alphabeta(state_copy, 3, -inf, inf, True, heuristic, times)
-    # print("\n")
-    # print(f"time to get moves: {times[0]}, time to remove illegal: {times[1]}")
-    # print(f"time to score: {times[2]}")
-    return len(moves), moves
+class GlobalStrategy:
+    def __init__(self, alphabeta_depth, heuristic):
+        self.alphabeta_depth = alphabeta_depth
+        self.heuristic = heuristic
 
-
+    def respond(self, state: State, time_message_received) -> Tuple[int, List]:
+        state_copy = state.copy_state()
+        times = [0, 0, 0]
+        _, moves, reduce_depth, times = alphabeta(state_copy, self.alphabeta_depth, -inf, inf, True, self.heuristic,
+                                    time_message_received, times)
+        if reduce_depth:
+            self.alphabeta_depth -= 1
+        # print("\n")
+        # print(f"time to get moves: {times[0]}, time to remove illegal: {times[1]}")
+        # print(f"time to score: {times[2]}")
+        return len(moves), moves
